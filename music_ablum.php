12026e675a66c37b7090f5999eb63e9ee97a2fe6
duxiangfei00 <382642582@qq.com>
2013-09-30 10:43:31 -0400

    This is the all ablums of girl's generation which is my music ablum.php

---
+++ music_ablum.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
+<title>Girl's Generation</title>
+<link href="Style3.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+.auto-style1 {
+	margin-left: 579px;
+}
+</style>
+</head>
+
+<body id="page2">
+	<p>
+	&nbsp;<?php include('front_menu.php'); ?><div id="header">
+	<h1>
+			<strong> Girl's Generation </strong>
+	</h1>
+	
+	<img alt="girl's orange "src="Images/Girls-Generation-Wallpaper-SNSD-Wallpaper-1.jpg" height="584" width="1337" />&nbsp;<table style="width: 1354px; height: 595px" id="table1" class="auto-style1">	 
+		 <tr>
+<th colspan="2" style="text-align:center;">Korean discography</th>
+<th colspan="2" style="text-align:center;">Japanese discography</th>
+
+		 </tr>
+		 <tr>
+<th style="text-align:center;">Date</th>
+<th style="text-align:center;" class="newStyle2">Name</th>
+<th style="text-align:center;">Date</th>
+<th style="text-align:center;" class="newStyle2">Name</th>
+         </tr>
+         <tr>
+<td style="text-align:center;">2007</td>
+<td style="text-align:center;" class="newStyle2">Girl's Generation </td>
+<td style="text-align:center;">2011</td>
+<td style="text-align:center;" class="newStyle2">Girl's Generation </td>
+
+         </tr>
+         <tr>
+<td style="text-align:center;">2009</td>
+<td style="text-align:center;" class="newStyle2">Tell Me Your Wish </td>
+<td style="text-align:center;">2012</td>
+<td style="text-align:center;" class="newStyle2">Girls and Peace </td>
+
+         </tr>
+         <tr>
+<td style="text-align:center; height: 43px;">2009</td>
+<td style="text-align:center; height: 43px;" class="newStyle2">Gee </td>
+         </tr>
+         <tr>
+<td style="text-align:center;">2010</td>
+<td style="text-align:center;"><span class="newStyle2">Hoot </span> </td>
+         </tr>
+         <tr>
+<td style="text-align:center;">2010</td>
+<td style="text-align:center;" class="newStyle2">Oh! </td>
+         </tr>
+         <tr>
+<td style="text-align:center;">2011</td>
+<td style="text-align:center;" class="newStyle2">The Boys </td>
+         </tr>
+         <tr class="newStyle2">
+<td style="text-align:center;">2013</td>
+<td style="text-align:center;">I Got Boy </td>
+         </tr>
+         <tr>
+<td style="text-align:center;">2007</td>
+<td style="text-align:center;" class="newStyle2">Girl's Generation </td>
+         </tr>
+
+
+</table>		
+		
+	</div>
+	
+		<div id="footer" style="height: 118px">
+			<p>@2013 Girl's generation.</p>
+		</div>	
+</body>
+</html>
