a6534aa725a7acc9b77fefe3a9de1bbcf3887437
duxiangfei00 <382642582@qq.com>
2013-09-30 10:41:14 -0400

    This is my header.php, which is girl's generation.

---
+++ header.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<link type="text/css" href="../lesson9/styles.css" rel="stylesheet"/>
+<title>Untitled 1</title>
+</head>
+
+<body>
+<h1>
+Girl's generation
+</h1>
+
