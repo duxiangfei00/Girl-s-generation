6581d8c75b276dcca3b98d399213e304b790c32b
duxiangfei00 <382642582@qq.com>
2013-09-30 10:41:54 -0400

    you can login in to my page

---
+++ login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML><HEAD><title>Girl's generation</title></head>
+<META content="text/html; charset=utf-8" http-equiv=Content-Type>
+
+<link href="Style3.css" rel="stylesheet" type="text/css">
+
+<style>body{text-align: center;}
+.auto-style3 {
+	font-family: "Berlin Sans FB Demi";
+	font-size: x-large;
+	font-weight: bolder;
+	font-style: italic;
+	font-variant: small-caps;
+	text-transform: lowercase;
+	margin-left: 12px;
+}
+.auto-style4 {
+	font-family: "Berlin Sans FB Demi";
+	font-size: x-large;
+	font-weight: bolder;
+	font-style: italic;
+	font-variant: small-caps;
+	text-transform: lowercase;
+	margin-left: 17px;
+}
+</style>
+
+<BODY id="page9">
+
+<?php include('front_menu.php'); ?>
+
+<h1>
+
+<h1 id="header"><span id="header" class="newStyle2">Girl's generation</span></h1>
+
+    <DIV><span id="header" class="newStyle2"><LABEL id="header">Username</LABEL></span> 
+	
+	<INPUT name=username class="auto-style4" style="width: 218px; height: 33px"><span class="newStyle2">
+	
+	</span><span id="header" class="newStyle2"><LABEL id="header">Password</LABEL></span>
+		<INPUT name=password type=password class="auto-style3" style="width: 203px; height: 33px"><span class="newStyle2">
+		</span> </DIV>
+
+</h1>
+
+<FORM method=post action=validation.php>
+	
+<INPUT value="Login" type="submit" id="header"> </FORM>
+
+<a href="admins.htm">Still don't have an account?</a>
+
+<p>Have a good day!</p>
+
+<footer>
+
+@2013 Girl's generation.</footer>
+
+</BODY>
+
+</HTML>
