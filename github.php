1ca2dbe6b02aca50a7e444d7291b5e988e34726d
duxiangfei00 <382642582@qq.com>
2013-09-30 10:37:36 -0400

    This is the link for github

---
+++ github.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="icon" 
+      type="image/png" 
+      href="http://default.php?id=1/CJyIhci.png" />
+<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
+<title>Girl's Generation</title>
+<link href="Style3.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+.auto-style2 {
+	text-align: right;
+	word-spacing: 7px;
+	margin-left: 5px;
+	margin-right: 6px;
+	margin-top: 5px;
+	margin-bottom: 4px;
+}
+</style>
+</head>
+    <body id="page10" style="height: 629px">
+	<?php include('front_menu.php'); ?>
+	<a href="https://github.com/duxiangfei00?tab=repositories"><img alt="github" src="external folder/My Images/okal-eltocat.jpg" height="603" width="844" style="float: left" /></a>
+
+	<div id="content">
+	</div>
+	<h3><span id="header" class="newStyle2">Share your work to github!!</span></h3>
+	
+	<p class="auto-style2" style="height: 19px" >&copy;2013 Girl's generation.</p>
