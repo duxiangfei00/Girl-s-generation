2b842c049412bb89cefaeaa06716472232d6c183
duxiangfei00 <382642582@qq.com>
2013-09-30 10:23:58 -0400

    Create drama.php

---
+++ drama.php
@@ -0,0 +1,34 @@
+girl's generation
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
+<title>Girl's Generation</title>
+<link href="Style3.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+.auto-style1 {
+	border-style: solid;
+	border-width: 5px;
+	margin: 20px 400px;
+}
+</style>
+</head>
+<body id="page4">
+&nbsp;<p>
+<img alt="loverain" class="auto-style1" name ="loverain" src="Images/love-rain-ending-4-cut.jpg"   /></p>
+		<?php include('front_menu.php'); ?>
+	<div id="content">
+	<h3 class="loverain2">
+	   They also develop in the movies and drama</h3>
+	<p class="loverain1">
+	
+	
+	
+		&nbsp;<b>Yoona</b> 
+		not only is a very good singer, but also she is also a perfect actress which has a strong gift. She is a actress of drama (Love Rain).  Love Rain went for the highest price to Japan before it even broadcast in Korea.[40] It cost &#8361;450 million per episode, adding up to some &#8361;900 million (or US$10 million) in total.[41] It has since been exported to 12 countries in Asia and Europe including China, Hong Kong, Taiwan, Thailand, Malaysia, Vietnam, Philippines, Cambodia, and Singapore, recording around &#8361;115 billion in overseas sales.</p>
+		</div>
+		<div id="footer">
+			<p>&copy;2013 Girl's generation.</p>
+		</div>
+</body>
+</html>
