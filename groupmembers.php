aafea6e8917fa516dad038843725b188de5edb31
duxiangfei00 <382642582@qq.com>
2013-09-30 10:39:03 -0400

    There are the all group members of girl's generation

---
+++ groupmembers.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
+<title>Girl's Generation</title>
+<link href="Style3.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+.auto-style1 {
+	margin-right: 0px;
+}
+</style>
+</head>
+<body id="page6">
+<?php include('front_menu.php'); ?>	<div id="header">
+	<h1>
+			<strong> Girl's Generation </strong>
+	</h1>
+	<h2> Nine angels </h2>
+	</div>
+	<img src="Images/snsd-first-japan-tour-trading-card-girls-generation-snsd-22728148-1280-8001.jpg" class="auto-style1" width="1215" />
+	<table>	 
+		 <tr>
+<th colspan="2" style="text-align:center;">Stage name</th>
+<th colspan="2" style="text-align:center;">Full name</th>
+<th rowspan="2" style="text-align:center; width: 286px;">Date of birth</th>
+</tr>
+<tr>
+<th style="text-align:center; width: 201px;">Romanized</th>
+<th style="text-align:center; width: 214px;">Hangul</th>
+<th style="text-align:center;">Romanized</th>
+<th style="text-align:center; width: 202px;">Hangul</th>
+</tr>
+<tr>
+<td style="text-align:center; width: 201px;">Taeyeon</td>
+<td style="text-align:center; width: 214px;">&#53468;&#50672;</td>
+<td style="text-align:center;">Kim Tae-yeon</td>
+<td style="text-align:center; width: 202px;">&#44608;&#53468;&#50672;</td>
+<td style="text-align:center; width: 286px;"><span style="display:none"></span> March 9, 1989 </td>
+</tr>
+<tr>
+<td style="text-align:center; width: 201px;">Jessica</td>
+<td style="text-align:center; width: 214px;">&#51228;&#49884;&#52852;</td>
+<td style="text-align:center;">Jessica Jung, Jung Soo-yeon</td>
+<td style="text-align:center; width: 202px;">&#51228;&#49884;&#52852; &#51221;, &#51221;&#49688;&#50672;</td>
+<td style="text-align:center; width: 286px;"><span style="display:none"></span> April 18, 1989 </td>
+</tr>
+<tr>
+<td style="text-align:center; width: 201px;">Sunny</td>
+<td style="text-align:center; width: 214px;">&#50024;&#45768;</td>
+<td style="text-align:center;">Lee Soon-kyu</td>
+<td style="text-align:center; width: 202px;">&#51060;&#49692;&#44508;</td>
+<td style="text-align:center; width: 286px;"><span style="display:none"></span> May 15, 1989</td>
+</tr>
+<tr>
+<td style="text-align:center; width: 201px;">Tiffany</td>
+<td style="text-align:center; width: 214px;">&#54000;&#54028;&#45768;</td>
+<td style="text-align:center;">Stephanie Hwang, Hwang Mi-young</td>
+<td style="text-align:center; width: 202px;">&#49828;&#53580;&#54028;&#45768; &#54889;, &#54889;&#48120;&#50689;</td>
+<td style="text-align:center; width: 286px;"><span style="display:none"></span> August 1, 1989 </td>
+</tr>
+<tr>
+<td style="text-align:center; width: 201px;">Hyoyeon</td>
+<td style="text-align:center; width: 214px;">&#54952;&#50672;</td>
+<td style="text-align:center;">Kim Hyo-yeon</td>
+<td style="text-align:center; width: 202px;">&#44608;&#54952;&#50672;</td>
+<td style="text-align:center; width: 286px;"><span style="display:none"></span> September 22, 1989 </td>
+</tr>
+<tr>
+<td style="text-align:center; width: 201px;">Yuri</td>
+<td style="text-align:center; width: 214px;">&#50976;&#47532;</td>
+<td style="text-align:center;">Kwon Yu-ri</td>
+<td style="text-align:center; width: 202px;">&#44428;&#50976;&#47532;</td>
+<td style="text-align:center; width: 286px;"><span style="display:none"> </span>December 5, 1989 </td>
+</tr>
+<tr>
+<td style="text-align:center; width: 201px;">Sooyoung</td>
+<td style="text-align:center; width: 214px;">&#49688;&#50689;</td>
+<td style="text-align:center;">Choi Soo-young</td>
+<td style="text-align:center; width: 202px;">&#52572;&#49688;&#50689;</td>
+<td style="text-align:center; width: 286px;"><span style="display:none"> </span> February 10, 1990 </td>
+</tr>
+<tr>
+<td style="text-align:center; width: 201px;">Yoona</td>
+<td style="text-align:center; width: 214px;">&#50980;&#50500;</td>
+<td style="text-align:center;">Im Yoon-ah</td>
+<td style="text-align:center; width: 202px;">&#51076;&#50980;&#50500;</td>
+<td style="text-align:center; width: 286px;"><span style="display:none"></span> May 30, 1990 </td>
+</tr>
+<tr>
+<td style="text-align:center; width: 201px;">Seohyun</td>
+<td style="text-align:center; width: 214px;">&#49436;&#54788;</td>
+<td style="text-align:center;">Seo Joo-hyun</td>
+<td style="text-align:center; width: 202px;">&#49436;&#51452;&#54788;</td>
+<td style="text-align:center; width: 286px;"><span style="display:none"></span> June 28, 1991 </td>
+</tr>
+</table>
+		 <div id="footer">
+			<p>&copy;2013 Girl's generation.</p>
+		 </div>
+		 
+
+</body>
+</html>
