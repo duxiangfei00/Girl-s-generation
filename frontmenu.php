35553487b4c97782704330d77106a96e43e22995
duxiangfei00 <382642582@qq.com>
2013-09-30 10:37:01 -0400

    This is my nav bar.

---
+++ frontmenu.php
@@ -0,0 +1,23 @@
+
+<div id='cssmenu'>
+<ul>
+   <li class='active'><a href="default.php?id=1"><span>Girl's_generation</span></a></li>
+   <li class='has-sub'><a href="default.php?id=2"><span>Achievements</span></a>
+      <ul>
+         <li><a href="music_album.php"><span>Music_Album</span></a></li>
+         <li><a href="Drama.php"><span>Drama</span></a></li>
+         <li class='last'><a href="video.php"><span>Video</span></a></li>
+      </ul>
+   </li>
+   <li class='has-sub'><a href="Group_Members.php"><span>Group_Members</span></a>
+      <ul>
+         <li><a href="aboutme.php"><span>About ME</span></a></li>
+         <li class='last'><a href="default.php?id=3"><span>Contact Us</span></a></li>
+      </ul>
+   </li>
+   <li class='last'><a href="shops.php"><span>Our Projects</span></a></li>
+   <li class='last'><a href="github.php"><span>Github</span></a></li>
+   <li class='last'><a href="login.php"><span>Login</span></a></li>
+</ul>
+</div>
+
