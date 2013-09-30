ac130e5edc833c88adac470ebb38baa0bb7aad4a
duxiangfei00 <382642582@qq.com>
2013-09-30 10:52:13 -0400

    This is the video page

---
+++ video.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8" />
+<title>Girl's Generation</title>
+<link href="Style3.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+.auto-style1 {
+	margin-left: 435px;
+	margin-right: 232px;
+	margin-top: 0px;
+}
+.newStyle1 {
+	font-family: Nyala;
+	font-size: large;
+	font-weight: lighter;
+	font-style: oblique;
+	font-variant: small-caps;
+	text-transform: uppercase;
+	color: #FF0000;
+}
+</style>
+</head>
+<body id="page5">
+	<?php include('front_menu.php'); ?>
+	<div id="header">
+		<h1>
+			<strong id="header"> Girl's Generation </strong>
+	</h1>
+	<h2> Nine angels </h2>
+	</div>
+	
+	<div>
+   <iframe id="snsd" src="//www.youtube.com/embed/ScIpxucdmo0" frameborder="0" allowfullscreen id="I1" class="auto-style1" name="I1" style="width: 1023px; height: 542px" ></iframe>	
+	</div>
+	<div id="content">
+	
+	 <p>
+		  <b>Gee</b>
+	     is the lead single from the album Gee by K-pop girl group Girls' Generation. It was released on online music sites on January 5, 2009, with the physical release of the EP on January 7. It quickly went to #1 on various charts and eventually set a record for being #1 on KBS's music chart show Music Bank for 9 consecutive weeks.</p>
+		 
+		 <p>The music video for "Gee" was the most viewed K-pop video on YouTube from September 2011 to September 1, 2012, after surpassing a live performance of "Nobody" by Wonder Girls and until being in its turn surpassed by "Gangnam Style" by Psy. As of May 1 2013, "Gee" has gained over 101 million views on YouTube.[3]
+ 
+"Dancing Queen" was supposed to be the group's lead single/title track for their first Korean EP but due to its last minute copyright issues, the song was replaced by "Gee". "Dancing Queen" was later released as single on their fourth Korean album "I Got a Boy" on January 1, 2013.
+     </p>
+     </div>
+		 <div id="footer">
+			<p>&copy;2013 Girl's generation.</p>
+		 </div>
+</body>
+</html>
